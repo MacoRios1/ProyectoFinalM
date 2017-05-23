3efd458bbc36d4db42cddb366756712399506417
Marco Antonio Ríos García <marcoantonioriosgarcia2@hotmail.com>
2017-05-22 18:48:34 -0600

    Clase Gastos Info

---
+++ ProyectoFinalMacoRios/ProyectoFinalMacoRios/GastosInfo.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoFinalMacoRios
+{
+    public class GastosInfo
+    {
+        string IdentificacionG;
+        string NombreG;
+        string TipoDeRepartoG;
+
+        public string IdentificacionG1
+        {
+            get
+            {
+                return IdentificacionG;
+            }
+
+            set
+            {
+                IdentificacionG = value;
+            }
+        }
+
+        public string NombreG1
+        {
+            get
+            {
+                return NombreG;
+            }
+
+            set
+            {
+                NombreG = value;
+            }
+        }
+
+        public string TipoDeRepartoG1
+        {
+            get
+            {
+                return TipoDeRepartoG;
+            }
+
+            set
+            {
+                TipoDeRepartoG = value;
+            }
+        }
+    }
+}
