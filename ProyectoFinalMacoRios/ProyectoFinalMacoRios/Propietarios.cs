f1ceee91f8b879ec2e8d626b4ab21911eb62d731
Marco Antonio Ríos García <marcoantonioriosgarcia2@hotmail.com>
2017-05-22 18:58:54 -0600

    Clase Propietarios

---
+++ ProyectoFinalMacoRios/ProyectoFinalMacoRios/Propietarios.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoFinalMacoRios
+{
+    public class Propietarios
+    {
+        string NombrePropietario;
+        string NitPropietario;
+        string CorreoPropietario;
+
+        public string NombrePropietario1
+        {
+            get
+            {
+                return NombrePropietario;
+            }
+
+            set
+            {
+                NombrePropietario = value;
+            }
+        }
+
+        public string NitPropietario1
+        {
+            get
+            {
+                return NitPropietario;
+            }
+
+            set
+            {
+                NitPropietario = value;
+            }
+        }
+
+        public string CorreoPropietario1
+        {
+            get
+            {
+                return CorreoPropietario;
+            }
+
+            set
+            {
+                CorreoPropietario = value;
+            }
+        }
+    }
+}
