f4bbbd701a63301cb033fe7ee34140c06819f19b
Marco Antonio Ríos García <marcoantonioriosgarcia2@hotmail.com>
2017-05-22 18:45:48 -0600

    Clase Comunidad

--- ProyectoFinalMacoRios/ProyectoFinalMacoRios/Comunidad.cs
+++ ProyectoFinalMacoRios/ProyectoFinalMacoRios/Comunidad.cs
@@ -7,5 +7,47 @@ namespace ProyectoFinalMacoRios
 {
     public class Comunidad
     {
+        int Identificacion;
+        string NombreC;
+        string LugarC;
+
+        public int Identificacion1
+        {
+            get
+            {
+                return Identificacion;
+            }
+
+            set
+            {
+                Identificacion = value;
+            }
+        }
+
+        public string NombreC1
+        {
+            get
+            {
+                return NombreC;
+            }
+
+            set
+            {
+                NombreC = value;
+            }
+        }
+
+        public string LugarC1
+        {
+            get
+            {
+                return LugarC;
+            }
+
+            set
+            {
+                LugarC = value;
+            }
+        }
     }
 }
