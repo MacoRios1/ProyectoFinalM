f1160cf5adf6f77ca23422b03cc669048557e307
Marco Antonio Ríos García <marcoantonioriosgarcia2@hotmail.com>
2017-05-22 18:55:30 -0600

    Clase Propiedades

---
+++ ProyectoFinalMacoRios/ProyectoFinalMacoRios/Propiedades.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoFinalMacoRios
+{
+    public class Propiedades
+    {
+        string TipoPropiedad;
+        string CodigoPropiedad;
+        int AreaPropiedad;
+        string NitPropietario;
+        string PorcentajeGastoPorZona;
+        string InfoTipoPropiedad;
+        int NumeroDormitorios;
+        string NombreComercial;
+        string ActividadComercial;
+        string EstadoGaraje;
+        string Bodega;
+
+        public string TipoPropiedad1
+        {
+            get
+            {
+                return TipoPropiedad;
+            }
+
+            set
+            {
+                TipoPropiedad = value;
+            }
+        }
+
+        public string CodigoPropiedad1
+        {
+            get
+            {
+                return CodigoPropiedad;
+            }
+
+            set
+            {
+                CodigoPropiedad = value;
+            }
+        }
+
+        public int AreaPropiedad1
+        {
+            get
+            {
+                return AreaPropiedad;
+            }
+
+            set
+            {
+                AreaPropiedad = value;
+            }
+        }
+
+        public string NitPropietario1
+        {
+            get
+            {
+                return NitPropietario;
+            }
+
+            set
+            {
+                NitPropietario = value;
+            }
+        }
+
+        public string PorcentajeGastoPorZona1
+        {
+            get
+            {
+                return PorcentajeGastoPorZona;
+            }
+
+            set
+            {
+                PorcentajeGastoPorZona = value;
+            }
+        }
+
+        public string InfoTipoPropiedad1
+        {
+            get
+            {
+                return InfoTipoPropiedad;
+            }
+
+            set
+            {
+                InfoTipoPropiedad = value;
+            }
+        }
+
+        public int NumeroDormitorios1
+        {
+            get
+            {
+                return NumeroDormitorios;
+            }
+
+            set
+            {
+                NumeroDormitorios = value;
+            }
+        }
+
+        public string NombreComercial1
+        {
+            get
+            {
+                return NombreComercial;
+            }
+
+            set
+            {
+                NombreComercial = value;
+            }
+        }
+
+        public string ActividadComercial1
+        {
+            get
+            {
+                return ActividadComercial;
+            }
+
+            set
+            {
+                ActividadComercial = value;
+            }
+        }
+
+        public string EstadoGaraje1
+        {
+            get
+            {
+                return EstadoGaraje;
+            }
+
+            set
+            {
+                EstadoGaraje = value;
+            }
+        }
+
+        public string Bodega1
+        {
+            get
+            {
+                return Bodega;
+            }
+
+            set
+            {
+                Bodega = value;
+            }
+        }
+    }
+}
