e8b450d4b412c6d8770b8aa59c0f2e68104048b9
Marco Antonio Ríos García <marcoantonioriosgarcia2@hotmail.com>
2017-05-22 19:03:17 -0600

    Clase GastosCalculo

---
+++ ProyectoFinalMacoRios/ProyectoFinalMacoRios/GastosCalculo.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoFinalMacoRios
+{
+    public class GastosCalculo
+    {
+        string IdGasto;
+        string Descripcion;
+        int Importe;
+        string TipodeZonadeReparto;
+
+        public string IdGasto1
+        {
+            get
+            {
+                return IdGasto;
+            }
+
+            set
+            {
+                IdGasto = value;
+            }
+        }
+
+        public string Descripcion1
+        {
+            get
+            {
+                return Descripcion;
+            }
+
+            set
+            {
+                Descripcion = value;
+            }
+        }
+
+        public int Importe1
+        {
+            get
+            {
+                return Importe;
+            }
+
+            set
+            {
+                Importe = value;
+            }
+        }
+
+        public string TipodeZonadeReparto1
+        {
+            get
+            {
+                return TipodeZonadeReparto;
+            }
+
+            set
+            {
+                TipodeZonadeReparto = value;
+            }
+        }
+    }
+}
